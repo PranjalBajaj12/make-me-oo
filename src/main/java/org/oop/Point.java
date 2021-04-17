eb053cc2de6839b2896233db02ecb42ba347c7b4
pranjal.bajaj <pranjal.bajaj@pranjalbajajs-MacBook-Pro.local>
2021-04-17 19:42:27 +0530

    Add. functionality to get angle between two points

--- src/main/java/org/oop/Point.java
+++ src/main/java/org/oop/Point.java
@@ -4,22 +4,24 @@ public class Point {
     private final double x;
     private final double y;
 
-    public double getX() {
-        return x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
-    public double calculateDistance(Point end) {
-        double xCoordinateDistance = end.x - x;
-        double yCoordinateDistance = end.y - y;
+    public double calculateDistance(Point endPoint) {
+        double xCoordinateDistance = getCoordinateDistance(x, endPoint.x);
+        double yCoordinateDistance = getCoordinateDistance(y, endPoint.y);
         return Math.sqrt(Math.pow(xCoordinateDistance, 2) + Math.pow(yCoordinateDistance, 2));
+    }
+
+    private double getCoordinateDistance(double startCoordinate, double endCoordinate) {
+        return endCoordinate - startCoordinate;
+    }
+
+    public double getDirectionAngle(Point endPoint) {
+        double xCoordinateDistance = getCoordinateDistance(x, endPoint.x);
+        double yCoordinateDistance = getCoordinateDistance(y, endPoint.y);
+        return Math.atan2(yCoordinateDistance, xCoordinateDistance);
     }
 }

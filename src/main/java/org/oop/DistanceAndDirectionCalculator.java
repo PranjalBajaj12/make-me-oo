880a1469ce3db9b11a95cf99c1410d729647ecff
pranjal.bajaj <pranjal.bajaj@pranjalbajajs-MacBook-Pro.local>
2021-04-17 19:41:39 +0530

    Remove. method to get direction angle between two points

--- src/main/java/org/oop/DistanceAndDirectionCalculator.java
+++ src/main/java/org/oop/DistanceAndDirectionCalculator.java
@@ -2,9 +2,4 @@ package org.oop;
 
 public class DistanceAndDirectionCalculator {
 
-    public static double direction(Point from, Point to) {
-        double xDistance = to.getX() - from.getX();
-        double yDistance = to.getY() - from.getY();
-        return Math.atan2(yDistance, xDistance);
-    }
 }

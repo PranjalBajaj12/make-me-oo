3056e4b3134fa0f57d0ac0bd829f404af3f7a08b
pranjal.bajaj <pranjal.bajaj@pranjalbajajs-MacBook-Pro.local>
2021-04-17 19:08:35 +0530

    Add. functionality to calculate distance between two points

--- src/main/java/org/oop/Point.java
+++ src/main/java/org/oop/Point.java
@@ -1,27 +1,25 @@
 package org.oop;
 
 public class Point {
-    private double x;
-    private double y;
+    private final double x;
+    private final double y;
 
     public double getX() {
         return x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
     }
 
     public double getY() {
         return y;
     }
 
-    public void setY(double y) {
-        this.y = y;
-    }
-
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
+    }
+
+    public double calculateDistance(Point end) {
+        double xCoordinateDistance = end.x - x;
+        double yCoordinateDistance = end.y - y;
+        return Math.sqrt(Math.pow(xCoordinateDistance, 2) + Math.pow(yCoordinateDistance, 2));
     }
 }
